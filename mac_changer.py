75cbd1543321cf931e3b25b179d1396d5111eae7
root <root@localhost.localdomain>
2019-12-19 03:43:55 -0700

    Added Mac Changer Script

---
+++ mac_changer.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+#Author: Niraj Kumar Naik
+
+
+import subprocess
+import optparse
+
+
+def get_arguments():
+    parser = optparse.OptionParser()
+
+    parser.add_option("-i", "--interface", dest="interface",
+                      help="Interface to change its MAC Address")
+    parser.add_option("-m", "--mac", dest="new_mac", help="New MAC Address")
+    (options, arguments) = parser.parse_args()
+
+    if not options.interface:
+        parser.error("[-] Please specify an interface, use --help for more info.")
+    elif not options.new_mac:
+        parser.error("[-] Please specify a new MAC, use --help for more info.")
+    
+    return options
+    
+
+
+def change_mac(interface, new_mac):
+    print("[+] Changing MAC Address for " + interface + " to " + new_mac)
+
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+    subprocess.call(["ifconfig", interface, "up"])
+
+
+if __name__ == '__main__':
+    options = get_arguments()
+    change_mac(options.interface, options.new_mac)
